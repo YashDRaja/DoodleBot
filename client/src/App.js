9ba5c62c0ee25385d08bfb95c0c59100cfa254b0
nikashan02 <nikashan02@gmail.com>
2021-04-28 17:59:02 -0400

    Updated Drawing

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
-import React, { Component } from 'react';
-import * as tf from '@tensorflow/tfjs';
-import Canvas from './Canvas'
+import React from 'react';
+import Canvas from './Canvas';
 
 
-//const model = await tf.loadLayersModel('model/model.json')
-
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Canvas />
-      </div>
-    )
-  }
+const App  = () => {
+  return (
+    <div className="App">
+      <Canvas/>
+    </div>
+  )
 }
 
 export default App;
--- client/src/Canvas.js
+++ client/src/Canvas.js
@@ -1,56 +1,144 @@
-import React, { useState, useRef } from 'react';
-import { Stage, Layer, Line, Text } from 'react-konva';
-
+import React, { useState, useRef, useEffect } from 'react';
+import { Stage, Layer, Line, Rect } from 'react-konva';
+import * as tf from '@tensorflow/tfjs';
 
 const Canvas = props => {
   const [tool, setTool] = useState('pen');
   const [lines, setLines] = useState([]);
   const [coords, setCoords] = useState([]);
+  //const [tempCoords, setTempCoords] = useState([]);
+  const [model, setModel] = useState();
+  const loadModel = async () => {
+    console.log('loading');
+    let curmodel = await tf.loadLayersModel('https://raw.githubusercontent.com/zaidalyafeai/zaidalyafeai.github.io/master/sketcher/model2/model.json');
+    curmodel.predict(tf.zeros([1, 28, 28, 1]));
+    setModel(curmodel);
+  }
+  const loadClassNames = async () => {
+    let arr = [];
+    fetch('https://raw.githubusercontent.com/zaidalyafeai/zaidalyafeai.github.io/master/sketcher/model2/class_names.txt')
+    .then((r) => {return r.text()})
+    .then(text => {
+      const lst = text.split(/\n/);
+      for (var i = 0; i < lst.length - 1; i++) {
+        let symbol = lst[i];
+        arr[i] = symbol;
+      }
+      setClassNames(arr);
+    });
+  }
+  const [classNames, setClassNames] = useState();
   const isDrawing = useRef(false);
 
-  // const getMinBox = () => {
-  //   let coorX = coords.map((p) => {
-  //     return p.x
-  //   });
-  //   let coorY = coords.map((p) => {
-  //     return p.y
-  //   });
-  //   let min_coords = {
-  //     x: Math.min.apply(null, coorX),
-  //     y: Math.min.apply(null, coorY)
-  //   }
-  //   let max_coords = {
-  //     x: Math.max.apply(null, coorX),
-  //     y: Math.max.apply(null, coorY)
-  //   }
-  //   return {
-  //     min: min_coords,
-  //     max: max_coords
-  //   }
-  // }
+  const getMinBox = () => {
+    let coorX = coords.map((p) => {
+      return p.x
+    });
+    let coorY = coords.map((p) => {
+      return p.y
+    });
+    let min_coords = {
+      x: Math.min.apply(null, coorX),
+      y: Math.min.apply(null, coorY)
+    }
+    let max_coords = {
+      x: Math.max.apply(null, coorX),
+      y: Math.max.apply(null, coorY)
+    }
+    return {
+      min: min_coords,
+      max: max_coords
+    }
+  }
 
-  // const getImageData = () => {
-  //   const mbb = getMinBox();
-  //   const dpi = window.devicePixelRatio;
-  //   const imgData = canvas.contextContainer
-  //                         .getImageData(
-  //                           mbb.min.x * dpi,
-  //                           mbb.min.y * dpi,
-  //                           (mbb.max.x - mbb.min.x) * dpi,
-  //                           (mbb.max.y - mbb.min.y) * dpi);
-  //   return imgData;
-  // }
+  const getImageData = (e) => {
+    const mbb = getMinBox();
+    const dpi = window.devicePixelRatio;
+    const imgData = e.target.getStage()
+                            .getChildren()[0]
+                            .getCanvas()
+                            .getContext()
+                            .getImageData(
+                              mbb.min.x * dpi,
+                              mbb.min.y * dpi,
+                              (mbb.max.x - mbb.min.x) * dpi,
+                              (mbb.max.y - mbb.min.y) * dpi
+                            );
 
-  // const getFrame = () => {
-  //   if (coords.length >= 2) { 
-  //     const imgData = getImageData()
-  //     const pred = model.predict(preprocess(imgData)).dataSync()
-  //     const indices = findIndicesOfMax(pred, 5)
-  //     const probs = findTopValues(pred, 5)
-  //     const names = getClassNames(indices)
-  //     setTable(names, probs)
-  //   }
-  // }
+    for(var i = 0; i < imgData.data.length; i += 4) {
+        if (imgData.data[i] === 0 &&
+            imgData.data[i + 1] === 0 &&
+            imgData.data[i + 2] === 0 &&
+            imgData.data[i + 3] === 0) {
+
+          imgData.data[i] = 255;
+          imgData.data[i + 1] = 255;
+          imgData.data[i + 2] = 255;
+          imgData.data[i + 3] = 255;
+        }
+    }
+    return imgData;
+  }
+
+  const getFrame = (e) => {
+    if (coords.length >= 2) { 
+      const imgData = getImageData(e);
+      const pred = model.predict(preprocess(imgData)).dataSync();
+      const indices = findIndicesOfMax(pred, 5);
+      const probs = findTopValues(pred, 5);
+      const names = getClassNames(indices);
+      console.log(probs);
+      console.log(names);
+    }
+  }
+  
+  const getClassNames = (indices) => {
+    var output = [];
+    for (var i = 0; i < indices.length; i++)
+      output[i] = classNames[indices[i]];
+    return output;
+  }
+
+  const findIndicesOfMax = (inp, count) => {
+    var outp = [];
+    for (var i = 0; i < inp.length; i++) {
+      outp.push(i); // add index to output array
+      if (outp.length > count) {
+        outp.sort((a, b) => {
+            return inp[b] - inp[a];
+        }); // descending sort the output array
+        outp.pop(); // remove the last index (index of smallest element in output array)
+      }
+    }
+    return outp;
+  }
+
+  const findTopValues = (inp, count) => {
+    var outp = [];
+    let indices = findIndicesOfMax(inp, count)
+    // show 5 greatest scores
+    for (var i = 0; i < indices.length; i++)
+      outp[i] = inp[indices[i]]
+    return outp
+  }
+
+  function preprocess(imgData) {
+    return tf.tidy(() => {
+      //convert to a tensor
+      let tensor = tf.browser.fromPixels(imgData, 1);
+      
+      //resize 
+      const resized = tf.image.resizeBilinear(tensor, [28, 28]).toFloat();
+      
+      //normalize 
+      const offset = tf.scalar(255.0);
+      const normalized = tf.scalar(1.0).sub(resized.div(offset));
+
+      //We add a dimension to get a batch shape
+      const batched = normalized.expandDims(0);
+      return batched;
+    })
+  }
 
   const handleMouseDown = (e) => {
     isDrawing.current = true;
@@ -66,7 +154,15 @@ const Canvas = props => {
     }
     const stage = e.target.getStage();
     const point = stage.getPointerPosition();
-    setCoords(coords.concat({x: point.x, y: point.y}));
+
+    // if (tempCoords.length > 10) {
+    //   setCoords(coords.concat(tempCoords));
+    //   setTempCoords([]);
+    // }
+
+    // setTempCoords(tempCoords.concat({x: point.x, y: point.y}));
+    setCoords(coords => [...coords, {x: point.x, y: point.y}]);
+
     let lastLine = lines[lines.length - 1];
     // add point
     lastLine.points = lastLine.points.concat([point.x, point.y]);
@@ -77,17 +173,25 @@ const Canvas = props => {
   };
 
   const handleMouseUp = (e) => {
+    //setCoords(coords.concat(tempCoords));
+    //setTempCoords([]);
+    getFrame(e);
     isDrawing.current = false;
-    console.log(coords);
-    // console.log(e.target.getStage().getChildren()[0].getCanvas().getContext().getImageData(
-    //                             mbb.min.x * dpi,
-    //                             mbb.min.y * dpi,
-    //                             (mbb.max.x - mbb.min.x) * dpi,
-    //                             (mbb.max.y - mbb.min.y) * dpi));
   };
+
+  useEffect(() => {
+    loadModel();
+    loadClassNames();
+  }, [tool]);
+
+  const clearCanvas = () => {
+    setLines([]);
+    setCoords([]);
+  }
 
   return (
     <div>
+      <button onClick={clearCanvas}>Clear canvas</button>
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
@@ -96,12 +200,11 @@ const Canvas = props => {
         onMouseup={handleMouseUp}
       >
         <Layer>
-          <Text text="Just start drawing" x={5} y={30} />
           {lines.map((line, i) => (
             <Line
               key={i}
               points={line.points}
-              stroke="#df4b26"
+              stroke="#000000"
               strokeWidth={10}
               tension={0.5}
               lineCap="round"
